eff2cfaa782485e8a97053b83e22ef1bf8000a86
Erick <128732040+Erick-Iuri@users.noreply.github.com>
2023-04-27 11:27:31 -0300

    Add files via upload

--- Resources/index.js
+++ Resources/index.js
@@ -10,14 +10,17 @@ const MenuRetratil = document.querySelector('.Navegação-Menu-H');
 }; */
 
 window.onload=()=>{
+    
     const $ = document.querySelector.bind(document);
-const $All = document.querySelectorAll.bind(document);
+    const $All = document.querySelectorAll.bind(document);
 
-$('#menu').onclick=()=>{              $('#menu').classList.toggle('rotate')
-  $('.nav-page1').classList.toggle('transform')
-  $('.nav-page2').classList.toggle('transform')
-  $('.menu-line1').classList.toggle('rotate1')
-   $('.menu-line2').classList.toggle('rotate2')
+$('#menu').onclick=()=>{              
+    
+    $('#menu').classList.toggle('rotate')
+    $('.nav-page1').classList.toggle('transform')
+    $('.nav-page2').classList.toggle('transform')
+    $('.menu-line1').classList.toggle('rotate1')
+    $('.menu-line2').classList.toggle('rotate2')
 }
 };
 
@@ -25,3 +28,5 @@ $('#menu').onclick=()=>{              $('#menu').classList.toggle('rotate')
 document.querySelector('.thema-preto').addEventListener('click', () => {
     document.body.classList.toggle('dark');
 });
+
+
